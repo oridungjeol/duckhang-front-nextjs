a5da41af5c6aed73a61e1950c130fee210247820
Azamman327 <wjsdbdud327@naver.com>
2025-08-08 16:23:16 +0900

    Feat: tailwind 세팅 및 로그인 화면 초기 셋업

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import "./page.css";
-import kakaoLoginImg from "../../../public/kakao_login_large_wide.png";
+import kakaoLoginImg from "public/kakao_login_large_wide.png";
 import Image from "next/image";
 
 const handleKakaoLogin = () => {
@@ -11,14 +11,25 @@ const handleKakaoLogin = () => {
 export default function Test() {
   return (
     <>
-      <h1 className="swiper-title title-margin">duckhang</h1>
+      <div className="h-full flex flex-col justify-center align-center gap-y-8">
+        <div>
+          <div className="text-center text-5xl">덕행</div>
+          <div className="text-center">DuckHang</div>
+        </div>
+        <div className="flex justify-center">
+          <button className="w-sm" onClick={handleKakaoLogin}>
+            <Image src={kakaoLoginImg} alt="카카오 로그인" />
+          </button>
+        </div>
+      </div>
+      {/* <h1 className="swiper-title title-margin">duckhang</h1>
       <div className="login-wrap">
         <div className="social">
           <button className="kakao-login-button" onClick={handleKakaoLogin}>
             <Image src={kakaoLoginImg} alt="카카오 로그인" />
           </button>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
