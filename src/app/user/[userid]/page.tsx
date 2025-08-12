ca108473d8d21de282cd636c84562413af53ca92
Azamman327 <wjsdbdud327@naver.com>
2025-08-12 18:50:54 +0900

    Feat: post, review 페이지 레이아웃 설정

--- src/app/user/[userid]/page.tsx
+++ src/app/user/[userid]/page.tsx
@@ -1,31 +1,42 @@
 import Image from "next/image";
 import Link from "next/link";
 
+// import Post from "./post";
+import Review from "./review";
+
 export default function Profile() {
   return (
     <>
-      <div className="w-full min-h-(calc(100vh-124.62px)) background-[#f9f8f7] flex justify-center">
-        <div className="w-[85%] h-full flex justify-center items-center background-[#f9f8f7bb] rounded-[15px]">
-          <div className="w-full h-[20vh] flex mt-[10%]">
-            <div className="w-[19vh] h-[19vh] relative rounded-[50%] object-cover mr-[8%] ml-[5%]">
+      <div className="w-full min-h-(calc(100vh-124.62px)) bg-(--background) flex justify-center">
+        <div className="w-(--content-frame) h-full flex flex-col justify-center items-center bg-(--background)">
+          <div className="w-full flex flex-row mt-10">
+            <div className="w-[25vh] h-[25vh] relative rounded-lg object-cover mr-10">
               <Image
                 src="https://cdn-icons-png.flaticon.com/512/11820/11820363.png"
                 fill={true}
                 alt="profile"
               />
             </div>
-            <div className="w-[100% - 140px] h-[30vh] ml-[5%]">
+            <div className="w-(--content-frame-20vh) h-[25vh] mt-5 ml-5">
               <Link href="/user/update">
-                <button className="w-[120px] h-[45px] rounded-[11px] background-[#ff9a42] background-[#f9f8f7] cursor-pointer">
+                <button className="w-[120px] h-[45px] rounded-lg bg-(--point-orange) cursor-pointer">
                   프로필 수정
                 </button>
               </Link>
-              <div className="text-3xl font-bold text-[#222] mb-5">닉네임</div>
-              <div className="text-2xl font-normal text-[#222]">
-                ⭐️별점: 4.5
-              </div>
+              <div className="text-3xl font-bold mt-5">닉네임</div>
+              <div className="text-2xl font-normal mt-5">⭐️별점: 4.5</div>
             </div>
           </div>
+          <div className="w-full flex justify-center mt-10 border-b-2 border-(--point-orange)">
+            <button className="w-[120px] h-[45px] border-b-3 border-(--point-orange)">
+              게시글
+            </button>
+            <button className="w-[120px] h-[45px] border-b-3 border-(--point-orange)">
+              리뷰
+            </button>
+          </div>
+          {/* <Post /> */}
+          <Review />
         </div>
       </div>
     </>
